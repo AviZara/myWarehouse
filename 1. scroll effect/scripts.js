876a00ac4890a48b5ef7c3e3cfada94c994bede7
Yi Yuan <yi.yuan.new@gmail.com>
2018-05-02 14:52:10 +0300

    no message

---
+++ 1. scroll effect/scripts.js
@@ -0,0 +1,7 @@
+
+window.sr = ScrollReveal();
+
+//window.sr = ScrollReveal({during:2000});
+
+//animation 2 second and sequence interval 50 milliseconds
+sr.reveal('.fooReveal', { container: '.fooContainer',during:2000 },500);
